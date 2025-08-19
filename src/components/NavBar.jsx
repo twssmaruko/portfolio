51e2200e0cb2646417a50459dd49b39b5ce5546b
twssmaruko <pneumonia_129@yahoo.com>
2025-08-19 20:40:55 +1200

    edited navbar and added css

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,18 +1,26 @@
 import { Link, useLocation } from "react-router-dom";
 import { Menu } from "antd";
+import "./navbar.css";
 
 const items = [
   { key: "/", label: <Link to="/">Home</Link> },
   { key: "/projects", label: <Link to="/projects">Projects</Link> },
   { key: "/about", label: <Link to="/about">About</Link> },
-  { key: "/contact", label: <Link to="/contact">Contact</Link> }
+  { key: "/contact", label: <Link to="/contact">Contact</Link> },
 ];
 
 const NavBar = () => {
   const { pathname } = useLocation();
   return (
-    <div style={{ padding: 12, boxShadow: "0 2px 8px rgba(0,0,0,.06)" }}>
-      <Menu mode="horizontal" selectedKeys={[pathname]} items={items} />
+    // <div style={{ padding: 12, boxShadow: "0 2px 8px rgba(0,0,0,.06)" }}>
+    //   <Menu mode="horizontal" selectedKeys={[pathname]} items={items} />
+    // </div>
+    <div className="nav-container">
+      <div className="nav-row">
+        <div>Projects</div>
+        <div>About</div>
+        <div>Contact</div>
+      </div>
     </div>
   );
 };
